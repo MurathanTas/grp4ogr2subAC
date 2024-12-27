8587e70c75283040863c9275d6aa14bd780a1b71
selva maramaei <119269320+selvamaramaei@users.noreply.github.com>
2024-12-27 13:43:34 +0300

    Add files via upload
    command.h eklendi

---
+++ include/command.h
@@ -0,0 +1,21 @@
+/*
+* SAKARYA ÜNİVERSİTESİ 2024 GÜZ DÖNEMİ
+* İŞLETİM SİSTEMLERİ PROJE ÖDEVİ
+*
+* Grup no 4 - 2. Öğretim -A ve C Şubesi
+*
+* Grup üyeleri:
+* - Murathan Taş - 2C
+* - Buğra Akşit - 2C
+* - Selva Maramaei - 2A
+* - İrem Kabil - 2C
+* - Elif Yerlikaya - 2A
+*/
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#define BUFFER_SIZE 1024
+
+void execute_command(char *command);
+
+#endif

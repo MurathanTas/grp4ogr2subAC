154eaebb1855e9fe913cc311762899525763db30
Elif Yerlikaya <122370715+elifyerlikaya@users.noreply.github.com>
2024-12-27 13:48:13 +0300

    Update background.h

--- include/background.h
+++ include/background.h
@@ -1,3 +1,16 @@
+/*
+* SAKARYA ÜNİVERSİTESİ 2024 GÜZ DÖNEMİ
+* İŞLETİM SİSTEMLERİ PROJE ÖDEVİ
+*
+* Grup no 4 - 2. Öğretim -A ve C Şubesi
+*
+* Grup üyeleri:
+* - Murathan Taş - 2C
+* - Buğra Akşit - 2C
+* - Selva Maramaei - 2A
+* - İrem Kabil - 2C
+* - Elif Yerlikaya - 2A
+*/
 #ifndef BACKGROUND_H
 #define BACKGROUND_H
 
